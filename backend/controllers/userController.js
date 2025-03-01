e6d5b079f7fb254e7a2dc3a74eee486c7c499a39
siva <karthik1959r@gmail.com>
2025-03-01 07:53:07 +0530

    Fixed Issues of bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('../config/db'); // Ensure this points to your MySQL connection
 const User = require('../models'); // Ensure this is correctly defined
