139c7c6bd6f33098cb65b7ab4090aac7e482d13b
siva <karthik1959r@gmail.com>
2025-03-06 15:38:09 +0530

    Updated backend deployed url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Change this URL to your backend API
+    baseURL: 'https://railway-management-system-production.up.railway.app/api', // Change this URL to your backend API
 });
 
 export default instance;
